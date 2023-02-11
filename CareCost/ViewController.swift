5cc9e3e41cb39e8ad001883405592a19ee9602f3
fgao63 <fgao63@gatech.edu>
2023-02-11 01:06:27 -0500

    Initial Commit

---
+++ CareCost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CareCost
+//
+//  Created by Frank Gao on 2/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
