19b7e574f495082544a81d9d05b112bec606535b
fgao63 <fgao63@gatech.edu>
2023-02-11 03:18:03 -0500

    configure startButton, added Theme file

---
+++ CareCost/Theme.swift
@@ -0,0 +1,71 @@
+//
+//  Theme.swift
+//  CareCost
+//
+//  Created by Frank Gao on 2/11/23.
+//
+
+import UIKit
+
+class Theme {
+    public static let VIEW_BACKGROUND_COLOR = UIColor(hex: "#E8998DFF")
+    
+    public static let BUTTON_BACKGROUND_COLOR = UIColor(hex: "#723D46FF")
+    public static let BUTTON_TEXT_COLOR = UIColor(hex: "#F4F0BBFF")
+    public static let BUTTON_PADDING = CGFloat(10)
+    
+    public static let CORNER_RADIUS = CGFloat(10)
+    
+    // set in WelcomeViewController
+    public static var SCREEN_WIDTH: CGFloat = 0
+    public static var SCREEN_HEIGHT: CGFloat = 0
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+}
+
+extension UIButton {
+    
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        
+        let minimumSize: CGSize = CGSize(width: 1.0, height: 1.0)
+        
+        UIGraphicsBeginImageContext(minimumSize)
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(origin: .zero, size: minimumSize))
+        }
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.clipsToBounds = true
+        self.setBackgroundImage(colorImage, for: forState)
+    }
+}
--- CareCost/WelcomeViewController.swift
+++ CareCost/WelcomeViewController.swift
@@ -8,26 +8,52 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var logoPlaceholder: UILabel!
     @IBOutlet weak var welcomeTextView: UITextView!
     
     @IBOutlet weak var startButton: UIButton!
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureWelcome()
-        configureStart()
-    }
-
-    private func configureWelcome() {
+        Theme.SCREEN_WIDTH = self.view.frame.width
+        Theme.SCREEN_HEIGHT = self.view.frame.height
+        self.view.backgroundColor = Theme.VIEW_BACKGROUND_COLOR
         
+        configureWelcome()
+        configureStartButton()
     }
+    
+    override func viewDidLayoutSubviews() {
+        print(startButton.hasAmbiguousLayout)
+        print(startButton.titleLabel?.frame.width)
+        print(startButton.titleLabel?.frame.height)
+    }
+    
+    private func configureWelcome() {
+        //welcomeLabel.font =
+    }
+    
+    private func configureStartButton() {
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: Theme.SCREEN_WIDTH / 3).isActive = true
+        startButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -Theme.SCREEN_WIDTH / 3).isActive = true
+        startButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        startButton.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 10).isActive = true
+                
+        let mySelectedAttributedTitle = NSAttributedString(string: "Start!",
+                                                           attributes: [NSAttributedString.Key.font : UIFont(name: "KannadaSangamMN-Bold", size: 28)!,
+                                                                        NSAttributedString.Key.foregroundColor : Theme.BUTTON_TEXT_COLOR!])
+        startButton.setAttributedTitle(mySelectedAttributedTitle, for: .normal)
+        startButton.setBackgroundColor(color: Theme.BUTTON_BACKGROUND_COLOR!, forState: .normal)
+        startButton.titleLabel!.textAlignment = .center
 
-    private func configureStart() {
+        startButton.layer.cornerRadius = Theme.CORNER_RADIUS
         startButton.addTarget(self, action: #selector(toInputVC), for: .touchUpInside)
     }
     
